3f2a1e0a090662e9769de32b8d440045ee1e7ca3
Bumbilo <dimagolyak@gmail.com>
2018-07-30 15:02:11 +0300

    added structure of app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { Car, Cars } from './car.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  public cars: Car[] = [
+    new Car('Ford', '12.12.12', 'Focus', false, 1),
+    new Car('Audi', '08.06.12', 'A4', true, 2),
+  ];
+
+  onAdd(car: Car) {
+    this.cars.push(car);
+  }
+
+  onDelete(car: Car) {
+    this.cars = this.cars.filter(c => c.id !== car.id);
+  }
 }
