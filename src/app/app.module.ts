09793d3195677da54d9435bab9028f27af4fd797
Bumbilo <dimagolyak@gmail.com>
2018-07-31 11:13:28 +0300

    added redux dev tools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { StoreModule } from '@ngrx/store';
 import { HttpClientModule } from '@angular/common/http';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppComponent } from './app.component';
 import { CarsFormComponent } from './cars-form/cars-form.component';
@@ -11,6 +14,7 @@ import { carsReducer } from './redux/cars.reducer';
 import { CarsService } from './cars.service';
 import { EffectsModule } from '@ngrx/effects';
 import { CarsEffect } from './redux/cars.effect';
+import { environment } from '../environments/environment.prod';
 
 
 @NgModule({
@@ -20,11 +24,14 @@ import { CarsEffect } from './redux/cars.effect';
     CarComponent
   ],
   imports: [
-    BrowserModule,
     FormsModule,
-    EffectsModule.forRoot([CarsEffect]),
+    BrowserModule,
     HttpClientModule,
-    StoreModule.forRoot({carPage: carsReducer})
+    EffectsModule.forRoot([CarsEffect]),
+    StoreModule.forRoot({carPage: carsReducer}),
+    RouterModule.forRoot([{path: '', component: AppComponent}]),
+    StoreRouterConnectingModule,
+    environment.production ? [] : StoreDevtoolsModule.instrument()
   ],
   providers: [CarsService],
   bootstrap: [AppComponent]
